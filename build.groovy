3368e6f551ebe4e782ae5d6b8f6871dc63eb615d
Derek DeJonghe <derek.dejonghe@rightbrainnetworks.com>
2017-10-22 15:08:28 -0400

    build typo

--- build.groovy
+++ build.groovy
@@ -28,7 +28,7 @@ node() {
     stage('deploy') {
         dir('picamtrigger'){
             sshagent(credentials: ['PiDeploy']) {
-              sh 'scp dist/picamtrigger-0.0.0.tar.gz  -o StrictHostKeyChecking=no pi@192.168.1.137:/home/pi/'
+              sh 'scp dist/picamtrigger-0.0.0.tar.gz  -o "StrictHostKeyChecking no" pi@192.168.1.137:/home/pi/'
               sh 'ssh pi@192.168.1.137 -o StrictHostKeyChecking=no "sudo pip3.6 install picamtrigger-0.0.0.tar.gz --force-reinstall --upgrade"'
             }
         }
